0c59b77bd9413454e7910eb111c34fafe90ffbd9
dibya.borah <dibya.borah@taazaa.com>
2024-06-05 01:01:16 +0530

    basic input form is created

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+let renderCount = 0;
+
 const App = () => {
+  renderCount++;
   return (
-    <div>App</div>
+    <div>
+      <div>
+        <button type="button">{renderCount}</button>
+      </div>
+      <form>
+        <div>
+          <input name="firstname" placeholder="FirstName" />
+        </div>
+        <div>
+          <input name="lastname" placeholder="LastName" />
+        </div>
+        <input type="submit" />
+      </form>
+    </div>
   )
 }
 
