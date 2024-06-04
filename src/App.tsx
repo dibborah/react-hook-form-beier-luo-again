a6eaf86dce795fa66672bb02755eb3fe0387dc3e
dibya.borah <dibyajyotiborah469@gmail.com>
2024-06-05 01:51:29 +0530

    learned errors,  handleSubmit, watch, register, providing  types and default values. Rendering variable using the watch API. Putting validation and displayer validation messages using errors state

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,50 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useForm } from "react-hook-form";
+
 let renderCount = 0;
 
+type FieldValues = {
+  firstname: string;
+  lastname: string;
+}
+
 const App = () => {
+  const { register, handleSubmit, formState: { errors }, watch } = useForm<FieldValues>({
+    defaultValues: {
+      firstname: 'bill',
+      lastname: 'luo',
+    }
+  });
   renderCount++;
+  const onsubmit = (data: any) => {
+    console.log(data);
+  }
+  console.log('errors', errors);
+  const firstName = watch('firstname');
+  console.log(firstName);
   return (
     <div>
       <div>
         <button type="button">{renderCount}</button>
       </div>
-      <form>
+      <p>FIRSTNAME: {firstName}</p>
+      <form onSubmit={handleSubmit(onsubmit)}>
         <div>
-          <input name="firstname" placeholder="FirstName" />
+          <input {...register("firstname", {
+            required: 'This is required'
+          })} placeholder="FirstName" />
+          {errors?.firstname && <div style={{ color: "red" }}>{errors?.firstname?.message}</div>}
         </div>
         <div>
-          <input name="lastname" placeholder="LastName" />
+          <input {...register("lastname", {
+            required: 'This is required',
+            minLength: {
+              value: 3,
+              message: 'Min length of 3 is necessary'
+            },
+          })} placeholder="LastName"
+          />
+          {errors?.lastname && <div style={{ color: "red" }}>{errors?.lastname?.message}</div>}
         </div>
         <input type="submit" />
       </form>
