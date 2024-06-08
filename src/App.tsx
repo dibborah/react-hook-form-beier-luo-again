b3513f59481f87d7cc64ea04328731414e403852
dibya.borah <dibyajyotiborah469@gmail.com>
2024-06-08 15:32:28 +0530

    Completed watch api of useForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 
 type FormFields = {
   firstname: string;
   lastname: string;
   age: number;
-}
+};
 
 let renderCount = 0;
 const App = () => {
-  const { register, handleSubmit, watch } = useForm<FormFields>();
+  const { register, handleSubmit, watch } = useForm<FormFields>({
+    defaultValues: {
+      firstname: 'bill',
+      lastname: 'luo',
+      age: 6,
+    },
+  });
   renderCount++;
   const onsubmit = (data: any) => {
     console.log(data);
   }
+  // console.log(watch(['firstname', 'lastname', 'age']));
+
+  // useEffect(() => {
+  //   const subscription = watch((data) => {
+  //     console.log(data);
+  //   });
+  //   return () => {
+  //     subscription.unsubscribe();
+  //   }
+  // }, [watch]);
+
+  // Watch is better to use Inside the render than inside useEffect
+  // So watch is always to be used inside the render function not inside the useEffect
+  // That is the reccomended way
+
+  const firstname = watch('firstname');
   return (
     <div>
       <div>
         <button type="button">{renderCount}</button>
       </div>
+      <p>
+        {firstname === 'bill' ? 'wait' : 'donot wait'}
+      </p>
       <form onSubmit={handleSubmit(onsubmit)}>
+        <br />
         <input {...register('firstname')} placeholder="FirstName" />
+        <br />
+        <input {...register('lastname')} placeholder="lastname" />
+        <br />
+        <input {...register('age')} placeholder="age" />
         <br />
         <br />
         <input type="submit" />
