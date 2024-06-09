6fdab069f8f1e7fe5ee47806022bc02961e5f792
dibya.borah <dibyajyotiborah469@gmail.com>
2024-06-09 14:53:37 +0530

    completed trigger api

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@
 import { useForm } from "react-hook-form";
 
 type FormFields = {
-    firstName: string,
-    lastName: string,
+  firstName: string,
+  lastName: string,
 };
 
 let renderCount = 0;
@@ -11,6 +11,8 @@ const App = () => {
   const {
     register,
     handleSubmit,
+    trigger,
+    formState: { errors },
   } = useForm<FormFields>({
     defaultValues: {
       firstName: '',
@@ -23,6 +25,7 @@ const App = () => {
     console.log(data);
   }
 
+  console.log('errors', errors);
   return (
     <div>
       <div>
@@ -30,14 +33,51 @@ const App = () => {
       </div>
       <form onSubmit={handleSubmit(onsubmit)}>
         <br />
-        <input {...register('firstName')}
+        <input {...register('firstName', {
+          required: true,
+          minLength: 6,
+        })}
           placeholder="FirstName"
         />
         <br />
-        <input {...register('lastName')}
+        <br />
+        <input {...register('lastName', {
+          required: true,
+          minLength: 6,
+        })}
           placeholder="LastName"
         />
         <br />
+        
+        {/* Calling trigger validate api on single fields multiple fields and on the 
+        entire form */}
+
+        {/* <button type="button" onClick={() => {
+          // trigger('firstName')
+          // trigger(['firstName', 'lastName'])
+          trigger()// Triggering the validation for the entire form
+        }}>trigger</button> */}
+        
+        {/* Calling trigger asynchronously */}
+
+        {/* <button type="button"
+          onClick={async () => {
+            const output = await trigger();
+            console.log('output', output);
+          }}
+        >
+          trigger
+        </button> */}
+        <button type="button"
+          onClick={async () => {
+            const output = await trigger('lastName', {
+              shouldFocus: true,
+            });
+            console.log('output', output);
+          }}
+        >
+          trigger
+        </button>
         <br />
         <input type="submit" />
       </form>
