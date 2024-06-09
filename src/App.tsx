66395dfb9c28d2daf2d52cf050ab5afb38ccc5e2
dibya.borah <dibyajyotiborah469@gmail.com>
2024-06-09 14:10:22 +0530

    continuing on reset Values

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 
 type FormFields = {
     firstName: string,
+    lastName: string,
 };
 
 let renderCount = 0;
@@ -10,9 +12,14 @@ const App = () => {
   const {
     register,
     handleSubmit,
+    reset,
+    formState,
+    getValues,
+    setValue,
   } = useForm<FormFields>({
     defaultValues: {
-      firstName: ''
+      firstName: '',
+      lastName: ''
     },
   });
 
@@ -21,6 +28,19 @@ const App = () => {
     console.log(data);
   }
 
+  console.log('isDirty', formState.isDirty);
+
+  // useEffect(() => {
+  //   if(formState.isSubmitSuccessful){
+  //     reset({
+  //       firstName: 'bill101',
+  //       lastName: 'luo101',
+  //     })
+  //   }
+  // }, [formState, reset]);
+
+  console.log('isSubmittedSuccessful', formState.isSubmitSuccessful);
+
   return (
     <div>
       <div>
@@ -28,12 +48,33 @@ const App = () => {
       </div>
       <form onSubmit={handleSubmit(onsubmit)}>
         <br />
-        <input {...register('firstName', {
-          required: true,
-        },)}
+        <input {...register('firstName')}
           placeholder="FirstName"
         />
         <br />
+        <input {...register('lastName')}
+          placeholder="LastName"
+        />
+        <br />
+        {/* <button type="button" onClick={() => {
+          reset({
+            ...getValues(),
+            lastName: 'luo + bill'
+          },{
+            // keepDefaultValues: true
+          });
+        }}>Reset</button> */}
+        <button onClick={() => {
+          setValue('firstName', 'billSetValue');
+          setValue('lastName', 'luoSetValeue');
+        }}>SetValue</button>
+        <br />
+        <button onClick={() => {
+          reset({
+            firstName: 'billReset',
+            lastName: 'luoReset'
+          })
+        }}>Reset</button>
         <br />
         <input type="submit" />
       </form>
