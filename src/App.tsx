7d38df80b22c8975278742992a167535024e72e9
dibya.borah <dibyajyotiborah469@gmail.com>
2024-06-13 04:04:45 +0530

    Started handleSubmit

--- src/App.tsx
+++ src/App.tsx
@@ -2,91 +2,53 @@ import { useForm } from "react-hook-form";
 
 type FormFields = {
   firstName: string;
-  customError: string;
 };
+
+const sleep = async (ms: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 
 let renderCount = 0;
 const App = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
-    setError,
   } = useForm<FormFields>({
     defaultValues: {
       firstName: '',
     },
-    // criteriaMode: 'all',
   });
 
   renderCount++;
-  const onsubmit = async (data: FormFields) => {
-    console.log(data);
-  }
 
-  console.log('errors', errors);
+  const onError = () => {
+    console.log('Wrong');
+  };
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
+    const onsubmit = async (data: FormFields, event: any) => {
+      // await sleep(2000)
+      console.log('data', data);
+      // console.log("Event", event);
+    }
+
   return (
     <div>
       <div>
         <button type="button">{renderCount}</button>
       </div>
-      <form onSubmit={handleSubmit(onsubmit)}>
+      {/* <form onSubmit={handleSubmit(onsubmit, onError)}> */}
+      <form onSubmit={(e) => handleSubmit(onsubmit)(e)}>
         <br />
-        <input {...register('firstName')}
+        <input {...register('firstName', {
+          // disabled: true,// If disable is set to true than result are going to be undefined
+          // plus inputs are now cannot be entered anymore
+        })}
           placeholder="FirstName"
         />
         <br />
-        <br />
-
-        {/* Single custom error message */}
-        {/* <button type="button"
-          onClick={() => {
-            setError('firstName', {
-              type: 'custom',
-              message: 'Something went wrong'
-            })
-          }}
-        >
-          setError
-        </button> */}
-
-        {/* Multiple custom error message */}
-        {/* <button
-          type="button"
-          onClick={() => {
-            setError('firstName', {
-              types: {
-                test: 'test went wrong',
-                test1: 'test1 went wrong',
-              }
-            })
-          }}
-        >
-          setError
-        </button> */}
-
-        {/* Creating custom error validation */}
-        {/* <button type="button"
-        onClick={() => {
-          setError("customError", {
-            type: 'server side',
-            message: 'server side error'
-          })
-        }}>setError</button>
-        <br /> */}
-        <button type="button"
-        onClick={() => {
-          setError("firstName", {
-            type: 'server side',
-            message: 'server side error'
-          }, {
-            shouldFocus: true,
-          })
-        }}>setError</button>
-        <br />
-        {/* <p>{errors.firstName?.types?.test}</p>
-        <p>{errors.firstName?.types?.test1}</p> */}
-        <p>{errors.customError?.message}</p>
         <br />
         <input type="submit" />
       </form>
