9fe1898d2be3a051bfc4153dbe1fe038ee3b385a
dibya.borah <dibyajyotiborah469@gmail.com>
2024-06-13 23:38:09 +0530

    Starting resetField

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,11 @@ type FormFields = {
   firstName: string;
 };
 
-// const sleep = async (ms: number) => {
-//   return new Promise((resolve) => {
-//     setTimeout(resolve, ms);
-//   });
-// }
-
 let renderCount = 0;
 const App = () => {
   const {
     register,
     handleSubmit,
-    // setError,
   } = useForm<FormFields>({
     defaultValues: {
       firstName: '',
@@ -24,24 +17,8 @@ const App = () => {
 
   renderCount++;
 
-  const onError = () => {
-    console.log('Wrong');
-  };
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
-  const onSubmit = async (data: FormFields, event: any) => {
-    // try {
-    //   await sleep(2000)
-    //   console.log('data', data);
-    // } catch (e) {
-    //   setError('service', {
-    //     type: 'Server side Error',
-    //     message: 'Something went wrong'
-    //   });
-    // }
-    // throw new Error('test');
+  const onSubmit = async (data: FormFields) => {
     console.log(data);
-    // console.log("Event", event);
   };
 
   return (
@@ -49,30 +26,14 @@ const App = () => {
       <div>
         <button type="button">{renderCount}</button>
       </div>
-      {/* <form onSubmit={handleSubmit(onSubmit, onError)}> */}
-      <form onSubmit={(e) => handleSubmit(onSubmit, onError)(e).catch(() => {
-        console.log('e', e);
-      })}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <br />
-        <input {...register('firstName', {
-          // disabled: true,// If disable is set to true than result are going to be undefined
-          // plus inputs are now cannot be entered anymore
-          // required: true,
-        })}
+        <input {...register('firstName')}
           placeholder="FirstName"
         />
         <br />
         <br />
         <input type="submit" />
-        <br />
-        <button
-          type="button"
-          onClick={(e) => {
-            handleSubmit(onSubmit, onError)(e);
-          }}
-        >
-          Fake Submit
-        </button>
         <br />
       </form>
     </div>
