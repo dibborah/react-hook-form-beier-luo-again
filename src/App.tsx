0e7044256bd3d8b71ccf7ebdf73e83bad2a3ae7c
dibya.borah <dibyajyotiborah469@gmail.com>
2024-06-16 15:01:23 +0530

    continuing useField array. Completed using the append, prepend, remove, field method from useFieldArray custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,39 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useEffect, useState } from "react";
-import { useForm, useFormState, useWatch } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
 
 type FormFields = {
-  firstName: string;
-  lastName: string;
+  cart: {
+    name: string;
+    amount: number;
+  }[]
 };
 
-const Controller = ({ control, register, render, name, rules }: any) => {
-  const props = register(name, rules);
-  const value = useWatch({
-    control,
-    name,
-  })
-  const { errors } = useFormState({
-    control,
-    name,
-  })
-
-  console.log('errors', errors);
-  return render({
-    value,
-    onChange: (e: any) => props.onChange({
-      target: {
-        name,
-        value: e.target.value,
-      }
-    }),
-    onBlur: props.onBlur,
-    name: props.name,
-  })
-}
-
-const Input = (props: any) => {
-  const [value, setValue] = useState(props.value || '');
-
-  React.useEffect(() => {
-    setValue(props.value);
-  }, [props.value]);// Asynchronous 'duo' update is done using this useEffect
-
-  return <input
-    onChange={(e) => {
-      setValue(e.target.value)
-      props.onChange && props.onChange(e)
-    }}
-    value={value}
-  />
-}
 let renderCount = 0;
 const App = () => {
   const {
     register,
     handleSubmit,
     control,
-    setValue,
-    // formState: {errors}
   } = useForm<FormFields>({
     defaultValues: {
-      firstName: '',
-      lastName: 'test',// This is working because of using the useWatch hook
+      cart: [{
+        name: '',
+        amount: 0,
+      }]
     },
   });
+
+  const { fields, append, prepend, remove } = useFieldArray({
+    name: 'cart',
+    control,
+  })
 
   renderCount++;
 
   const onSubmit = async (data: FormFields) => {
     console.log(data);
   };
-  
-  // console.log('errors', errors);
 
-  // setValue('lastName', 'bill');
-
-  React.useEffect(() => {
-    setTimeout(() => {
-      setValue('lastName', 'duo');// This line or useEffect is woking becuase of the useEffect in <Input /> component
-    }, 1000)
-  }, [setValue]);
   return (
     <div>
       <div>
@@ -84,19 +41,40 @@ const App = () => {
       </div>
       <form onSubmit={handleSubmit(onSubmit)}>
         <br />
-        <input autoComplete="off" {...register('firstName')}
-          placeholder="FirstName"
-        />
-        <br />
-        <Controller {...{
-          register,
-          control,
-          rules: {
-            required: true
-          },
-          name: 'lastName',
-          render: (props: any) => <Input {...props} />
-        }} />
+        {fields.map((item, index) => {
+          return (
+            <section key={index}>
+              <label>
+                <span>Name</span>
+                <input type="text" {...register(`cart.${index}.name`)} />
+              </label>
+              <br />
+              <br />
+              <label>
+                <span>Amount</span>
+                <input type="number" {...register(`cart.${index}.amount`, { valueAsNumber: true })} />
+              </label>
+              <br />
+              <br />
+              <button type="button" onClick={() => {
+                append({
+                  name: 'append',
+                  amount: 0,
+                })
+              }}>Append</button>
+              <button type="button" onClick={() => {
+                prepend({
+                  name: 'prepend',
+                  amount: 0,
+                })
+              }}>Prepend</button>
+              <button type="button" onClick={() => {
+                remove(index)
+              }}>Delete</button>
+              <hr />
+            </section>
+          )
+        })}
         <br />
         <input type="submit" />
         <br />
